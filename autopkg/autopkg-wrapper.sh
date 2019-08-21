500cfb0ddaff51406b17d59f9666bd0243f18a73
bryanheinz <git@bryanheinz.com>
2019-08-21 12:38:21 -0500

    converted  to , moved quotes from the verables to the whole path

--- autopkg/autopkg-wrapper.sh
+++ autopkg/autopkg-wrapper.sh
@@ -12,7 +12,7 @@ autopkg_user="autopkg"
 
 # define logger behavior
 logger="/usr/bin/logger -t autopkg-wrapper"
-user_home_dir=`dscl . -read /Users/${autopkg_user} NFSHomeDirectory | awk '{ print $2 }'`
+user_home_dir=$(dscl . -read /Users/${autopkg_user} NFSHomeDirectory | awk '{ print $2 }')
 
 # run autopkg
 if [ "${1}" == "help" ]; then
@@ -25,17 +25,17 @@ elif [ "${1}" == "initialize" ]; then
     $logger "starting autopkg to initialize a new default output log"
     
     # make sure autopkg folder exists in autopkg_user's Documents folder
-    if [ ! -d "${user_home_dir}"/Documents/autopkg ]; then
-        /bin/mkdir -p "${user_home_dir}"/Documents/autopkg
+    if [ ! -d "${user_home_dir}/Documents/autopkg" ]; then
+        /bin/mkdir -p "${user_home_dir}/Documents/autopkg"
     fi
     
     # make sure recipe list file exists
-    if [ ! -f "${user_home_dir}"/Documents/autopkg/recipe_list.txt ]; then
-        echo "MakeCatalogs.munki" >> "${user_home_dir}"/Documents/autopkg/recipe_list.txt
+    if [ ! -f "${user_home_dir}/Documents/autopkg/recipe_list.txt" ]; then
+        echo "MakeCatalogs.munki" >> "${user_home_dir}/Documents/autopkg/recipe_list.txt"
     fi
     
     # read the recipe list file
-    recipe_list_file=$(cat "${user_home_dir}"/Documents/autopkg/recipe_list.txt)
+    recipe_list_file=$(cat "${user_home_dir}/Documents/autopkg/recipe_list.txt")
     # combine the recipe list file with the hard-coded recipe list
     recipe_list="${recipe_list} ${recipe_list_file}"
     
@@ -50,14 +50,14 @@ elif [ "${1}" == "initialize" ]; then
     
     $logger "autopkg initial run to saved log location"
     echo "for this autopkg run, output will not be shown, but rather saved to default log location (${user_home_dir}/Documents/autopkg/autopkg.out"
-    /usr/local/bin/autopkg run ${recipe_list} 2>&1 > "${user_home_dir}"/Documents/autopkg/autopkg.out
+    /usr/local/bin/autopkg run ${recipe_list} 2>&1 > "${user_home_dir}/Documents/autopkg/autopkg.out"
     
     $logger "finished autopkg"
-elif [ ! -f "${user_home_dir}"/Documents/autopkg/autopkg.out ]; then
+elif [ ! -f "${user_home_dir}/Documents/autopkg/autopkg.out" ]; then
     # default log doesn't exist, so tell user to run this script in initialization mode and exit
     echo "ERROR: default log does not exist, please run this script with initialize argument to initialize the log"
     exit -1
-elif [ ! -f "${user_home_dir}"/Documents/autopkg/recipe_list.txt ]; then
+elif [ ! -f "${user_home_dir}/Documents/autopkg/recipe_list.txt" ]; then
     # default recipe doesn't exist, so tell user to run this script in initialization mode and exit
         echo "ERROR: default recipe list does not exist, please run this script with initialize argument to initialize the recipe list"
         exit -1
@@ -76,7 +76,8 @@ else
     $logger "finished autopkg"
     
     # check output against the saved log and if differences exist, send current log to specified recipient
-    if [ "`diff /tmp/autopkg.out \"${user_home_dir}\"/Documents/autopkg/autopkg.out`" != "" ]; then
+    log_diff=$(diff /tmp/autopkg.out "${user_home_dir}/Documents/autopkg/autopkg.out")
+    if [ "$log_diff" != "" ]; then
         # there are differences from a "Nothing downloaded, packaged or imported" run... might be an update or an error
         $logger "sending autopkg log"
         /usr/bin/mail -s "autopkg log" ${mail_recipient}    < /tmp/autopkg.out
