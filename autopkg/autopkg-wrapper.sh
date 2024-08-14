78599f02f38d616034d4a0c565485deea014aa94
bryan heinz <git@bryanheinz.com>
2024-08-14 15:55:04 -0500

    updated logging

--- autopkg/autopkg-wrapper.sh
+++ autopkg/autopkg-wrapper.sh
@@ -21,10 +21,14 @@ autopkg_tmp_file="/private/tmp/autopkg.out"
 # script path for emailing the results
 emailer="${autopkg_wrapper_root}/autopkg/emailer.py"
 
-# don't change anything below this line
+# don't change anything below this line #
 
-# define logger behavior
-logger="/usr/bin/logger -t autopkg-wrapper"
+# loosely logged in case there's a problem. won't survive reboots, but also
+# don't have to manage logging storage
+logger () {
+    echo "$1"
+    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") $1" >> /private/tmp/autopkg_wrapper.log
+}
 
 
 # make sure autopkg wrapper custom folders exist
@@ -41,7 +45,7 @@ if [[ "${1}" == "help" ]]; then
 elif [[ "${1}" == "initialize" ]]; then
     # initialize default log for automated run to check against for notification
     # if things have changed
-    $logger "starting autopkg to initialize a new default output log"
+    logger "starting autopkg to initialize a new default output log"
     
     # make sure recipe list file exists
     if [[ ! -f "${recipe_list_file}" ]]; then
@@ -52,52 +56,48 @@ elif [[ "${1}" == "initialize" ]]; then
     
     # run autopkg twice, once to get any updates and the second to get a log
     # indicating nothing changed
-    $logger "autopkg initial run to temporary log location"
-    echo "for this autopkg run, output will be shown"
+    logger "autopkg initial run to temporary log location"
+    logger "for this autopkg run, output will be shown"
     /usr/local/bin/autopkg run -v --recipe-list "$recipe_list_file" 2>&1
     
-    $logger "autopkg initial run to saved log location"
-    echo "for this autopkg run, output will not be shown, but rather saved to default log location (${autopkg_output_file})"
+    logger "autopkg initial run to saved log location"
+    logger "for this autopkg run, output will not be shown, but rather saved to default log location (${autopkg_output_file})"
     /usr/local/bin/autopkg run --recipe-list "$recipe_list_file" \
         2>&1 > "${autopkg_output_file}"
     
-    $logger "finished autopkg"
+    logger "finished autopkg"
 elif [[ ! -f "${autopkg_output_file}" ]]; then
     # default log doesn't exist, so tell user to run this script in
     # initialization mode and exit
-    echo "ERROR: default log does not exist, please run this script with initialize argument to initialize the log"
+    logger "ERROR: default log does not exist, please run this script with initialize argument to initialize the log"
     exit 1
 elif [[ ! -f "${recipe_list_file}" ]]; then
     # default recipe doesn't exist, so tell user to run this script in initialization mode and exit
-    echo "ERROR: default recipe list does not exist, please run this script with initialize argument to initialize the recipe list"
+    logger "ERROR: default recipe list does not exist, please run this script with initialize argument to initialize the recipe list"
     exit 1
 else
     # default is to just run autopkg and email log if something changed from normal
     
-    $logger "starting autopkg"
-    echo "starting autopkg"
+    logger "starting autopkg"
     /usr/local/bin/autopkg repo-update all
     /usr/local/bin/autopkg run --recipe-list "$recipe_list_file" \
         2>&1 > ${autopkg_tmp_file}
     
-    $logger "finished autopkg"
-    echo "finished autopkg"
+    logger "finished autopkg"
     
     # check output against the saved log and if differences exist, send current
     # log to specified recipient
     log_diff=$(/usr/bin/diff "$autopkg_tmp_file" "$autopkg_output_file")
     if [[ "$log_diff" != "" ]]; then
-        echo "emailing log."
         # there are differences from a "Nothing downloaded, packaged or
         # imported" run... might be an update or an error
-        $logger "sending autopkg log"
+        logger "emailing autopkg log"
         # email the log using a custom Python SMTP email script.
         # arg1 == TO address, arg2 == FROM address
         python3 $emailer $mail_to $mail_from
-        $logger "sent autopkg log to ${mail_to}, $(/usr/bin/wc -l "$autopkg_tmp_file" | /usr/bin/awk '{ print $1 }') lines in log"
+        logger "sent autopkg log to ${mail_to}, $(/usr/bin/wc -l "$autopkg_tmp_file" | /usr/bin/awk '{ print $1 }') lines in log"
     else
-        echo "not emailing log."
-        $logger "autopkg did nothing, so not sending log"
+        logger "no autopkg updates. not sending a log."
     fi
 fi
 exit 0
