21d5145fd6e78ab8a7721730467b5f7a387bf1da
bryan heinz <git@bryanheinz.com>
2024-08-14 15:21:21 -0500

    variable cleanup

--- autopkg/autopkg-wrapper.sh
+++ autopkg/autopkg-wrapper.sh
@@ -1,30 +1,36 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # autopkg automation script which, when run with no arguments, checks current run's output against a default output and sends the output to a user if there are differences
 
 # adjust the following variables for your particular configuration
 # you should manually run the script with the initialize option if you change the recipe list, since that will change the output.
-recipe_list=""
-mail_recipient="tickets@example.com"
-autopkg_user="autopkg"
+mail_to="tickets@example.com"
+mail_from="autopkg@example.com"
 
 # define file locations
-# folder path to store autopkg-wrapper files
-autopkg_wrapper_dir="/opt/autopkg/automation-scripts"
+# folder path to store autopkg-wrapper files (the root folder for this script)
+autopkg_wrapper_root="/opt/autopkg/automation-scripts"
+# folder path to store custom autopkg-wrapper files
+autopkg_wrapper_custom="${autopkg_wrapper_root}/custom"
 # text file with a list of recipes (easier than the above recipe_list)
-recipe_list_file="${autopkg_wrapper_dir}/recipe_list.txt"
+recipe_list_file="${autopkg_wrapper_custom}/recipe_list.txt"
 # persistent file path to store AutoPkg output for diffing
-autopkg_output_file="${autopkg_wrapper_dir}/autopkg.out"
+autopkg_output_file="${autopkg_wrapper_custom}/autopkg.out"
 # temporary file path to store AutoPkg output for diffing
 autopkg_tmp_file="/private/tmp/autopkg.out"
-
-emailer="${autopkg_wrapper_dir}/autopkg/emailer.py"
+# script path for emailing the results
+emailer="${autopkg_wrapper_root}/autopkg/emailer.py"
 
 # don't change anything below this line
 
 # define logger behavior
 logger="/usr/bin/logger -t autopkg-wrapper"
-user_home_dir=$(dscl . -read /Users/${autopkg_user} NFSHomeDirectory | awk '{ print $2 }')
+
+
+# make sure autopkg wrapper custom folders exist
+if [[ ! -d "$autopkg_wrapper_custom" ]]; then
+    /bin/mkdir -p "$autopkg_wrapper_custom"
+fi
 
 # run autopkg
 if [ "${1}" == "help" ]; then
@@ -33,29 +39,22 @@ if [ "${1}" == "help" ]; then
     echo "(initializes a new default log for notification checking)"
     exit 0
 elif [ "${1}" == "initialize" ]; then
-    # initialize default log for automated run to check against for notification if things have changed
+    # initialize default log for automated run to check against for notification
+    # if things have changed
     $logger "starting autopkg to initialize a new default output log"
-    
-    # make sure autopkg folder exists in autopkg_user's Documents folder
-    if [ ! -d "${autopkg_wrapper_dir}" ]; then
-        /bin/mkdir -p "${autopkg_wrapper_dir}"
-    fi
     
     # make sure recipe list file exists
     if [ ! -f "${recipe_list_file}" ]; then
         echo "MakeCatalogs.munki" >> "${recipe_list_file}"
     fi
     
-    # read the recipe list file
-    recipe_list_contents=$(cat "${recipe_list_file}")
     # combine the recipe list file with the hard-coded recipe list
-    recipe_list="${recipe_list} ${recipe_list_contents}"
+    recipe_list=$(cat "${recipe_list_file}")
     
     echo "recipe list: ${recipe_list}"
-    echo "autopkg user: ${autopkg_user}"
-    echo "user home dir: ${user_home_dir}"
     
-    # run autopkg twice, once to get any updates and the second to get a log indicating nothing changed
+    # run autopkg twice, once to get any updates and the second to get a log
+    # indicating nothing changed
     $logger "autopkg initial run to temporary log location"
     echo "for this autopkg run, output will be shown"
     /usr/local/bin/autopkg run -v ${recipe_list} 2>&1
@@ -68,18 +67,16 @@ elif [ "${1}" == "initialize" ]; then
 elif [ ! -f "${autopkg_output_file}" ]; then
     # default log doesn't exist, so tell user to run this script in initialization mode and exit
     echo "ERROR: default log does not exist, please run this script with initialize argument to initialize the log"
-    exit -1
+    exit 1
 elif [ ! -f "${recipe_list_file}" ]; then
     # default recipe doesn't exist, so tell user to run this script in initialization mode and exit
-        echo "ERROR: default recipe list does not exist, please run this script with initialize argument to initialize the recipe list"
-        exit -1
+    echo "ERROR: default recipe list does not exist, please run this script with initialize argument to initialize the recipe list"
+    exit 1
 else
     # default is to just run autopkg and email log if something changed from normal
     
     # read the recipe list file
-    recipe_list_contents=$(cat "${recipe_list_file}")
-    # combine the recipe list file with the hard-coded recipe list
-    recipe_list="${recipe_list} ${recipe_list_contents}"
+    recipe_list=$(cat "${recipe_list_file}")
     
     $logger "starting autopkg"
     echo "starting autopkg"
@@ -96,7 +93,8 @@ else
         # there are differences from a "Nothing downloaded, packaged or imported" run... might be an update or an error
         $logger "sending autopkg log"
         # email the log using a custom Python SMTP email script.
-        python3 $emailer
+        # arg1 == TO address, arg2 == FROM address
+        python3 $emailer $mail_to $mail_from
         $logger "sent autopkg log to {$mail_recipient}, $(wc -l "$autopkg_tmp_file" | awk '{ print $1 }') lines in log"
     else
         echo "not emailing log."
